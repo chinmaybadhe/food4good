d061451b3dbce69e6d653825c43f26c2e5aae4e0
Devendra Chaudhari <devendrachaudhari@Devendras-MBP.lan>
2022-04-10 02:12:54 -0700

    dashboard bug fix

--- app/src/main/java/com/example/food4good/ContributorListActivity.java
+++ app/src/main/java/com/example/food4good/ContributorListActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -44,13 +45,16 @@ import com.google.type.LatLngOrBuilder;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.TreeMap;
 
-public class ContributorListActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+public class ContributorListActivity extends AppCompatActivity{
 
     MyRecyclerViewAdapter adapter;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    MyRecyclerViewAdapter.ItemClickListener ctx;
+    //MyRecyclerViewAdapter.ItemClickListener ctx;
     private static final int REQUEST_LOCATION = 1;
     LocationManager locationManager;
     String latitude, longitude;
@@ -63,16 +67,23 @@ public class ContributorListActivity extends AppCompatActivity implements MyRecy
     ArrayList<Contributor> clist;
     ArrayList<AvailableFood> olist;
 
+    //dev
+    RecyclerView contributorListRecycleView;
+    ArrayList<AvailableFood> alAF;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
+        contributorListRecycleView = findViewById(R.id.contributorListRecycleView);
+        alAF = new ArrayList<>();
+
         setContentView(R.layout.activity_contributor_list);
 //        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.blue));
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.blue));
-        ctx=this;
+        //ctx=this;
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
 
@@ -130,6 +141,48 @@ public class ContributorListActivity extends AppCompatActivity implements MyRecy
         alertDialog.show();
     }
 
+    public static double distance(double lat1, double lat2, double lon1,
+                                  double lon2) {
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        //double height = el1 - el2;
+
+        distance = Math.pow(distance, 2);
+
+        return Math.sqrt(distance);
+    }
+
+    public void sort(ArrayList<Contributor> clist, ArrayList<AvailableFood> olist, String longitude, String latitude)
+    {
+        double lon = Double.parseDouble(longitude);
+        double lat = Double.parseDouble(latitude);
+
+        //remove contributor if it is not listed in olist
+        Log.d("SORT", String.valueOf(clist.size()));
+        Log.d("SORT", String.valueOf(olist.size()));
+
+        TreeMap<Double,Contributor> tm = new TreeMap<>();
+        for(Contributor c: clist)
+        {
+            Log.d("SORT",c.getId());
+            double dis = distance(lat,Double.parseDouble(c.getLatitude()),lon,Double.parseDouble(c.getLongitude()));
+            while(tm.containsKey(dis))
+                dis += 0.01;
+            tm.put(dis,c);
+        }
+        clist.clear();
+        for(Double d: tm.keySet())
+            clist.add(tm.get(d));
+    }
 
 
 
@@ -151,71 +204,51 @@ public class ContributorListActivity extends AppCompatActivity implements MyRecy
         Log.d("ContributorList",longitude);
         Log.d("ContributorList",latitude);
 
+        MyRecyclerViewAdapter afAdapter = new MyRecyclerViewAdapter(alAF,ContributorListActivity.this);
+        contributorListRecycleView = findViewById(R.id.contributorListRecycleView);
+        contributorListRecycleView.setAdapter(afAdapter);
+        contributorListRecycleView.setLayoutManager(new LinearLayoutManager(ContributorListActivity.this));
+
         conRefFood.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-
-
-                if(error!=null){
-                    Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
-                    return;
+                if(error!=null)
+                {
+                    //
                 }
-                olist = new ArrayList<>();
-                for (QueryDocumentSnapshot documentSnapshot : value) {
-                    AvailableFood c = documentSnapshot.toObject(AvailableFood.class);
-                    if(c.getVegMeals()>0 || c.getNonVegMeals()>0) olist.add(c);
-                    Log.d("Food", ""+olist.size());
-                }
-                Log.d("OnStart", String.valueOf(olist.size()));
-                clist= new ArrayList<>();
-                conRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
-                                Contributor c = documentSnapshot.toObject(Contributor.class);
-
-                                clist.add(c);
-
-                            }
-                            Log.d("Contributor", clist.size()+"");
-                            RecyclerView recyclerView = findViewById(R.id.contributorListRecycleView);
-                            recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                            adapter = new MyRecyclerViewAdapter(getApplicationContext(), olist,clist, mylocation);
-                            adapter.setClickListener(ContributorListActivity.this);
-                            recyclerView.setAdapter(adapter);
-                        } else {
-                            Log.d("onComplete", "Error getting documents: ", task.getException());
-                        }
+                else
+                {
+                    alAF.clear();
+                    for(QueryDocumentSnapshot documentSnapshot: value)
+                    {
+                        AvailableFood af = documentSnapshot.toObject(AvailableFood.class);
+                        alAF.add(af);
+                        afAdapter.notifyDataSetChanged();
                     }
-                });
-
-
-
-
-
+                }
             }
         });
+
     }
 
 
-    @Override
-    public void onItemClick(View view, int position) {
-//        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
-        Context context = view.getContext();
-        Intent intent = new Intent(context , RequestFoodActivity.class);
-        intent.putExtra("address",clist.get(position).getAddress());
-        intent.putExtra("name",clist.get(position).getName());
-        intent.putExtra("Id",clist.get(position).getId());
-        intent.putExtra("nonvegmeals",olist.get(position).getNonVegMeals()+"");
-        intent.putExtra("vegmeals",olist.get(position).getVegMeals()+"");
-        intent.putExtra("photoUrl",clist.get(position).getPhotoUrl());
-        intent.putExtra("latitude",clist.get(position).getLatitude());
-        intent.putExtra("longitude",clist.get(position).getLongitude());
-
-        System.out.println(clist.get(position)+" "+olist.get(position));
-        this.startActivity(intent);
-    }
+//    @Override
+//    public void onItemClick(View view, int position) {
+////        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+//        Context context = view.getContext();
+//        Intent intent = new Intent(context , RequestFoodActivity.class);
+//        intent.putExtra("address",clist.get(position).getAddress());
+//        intent.putExtra("name",clist.get(position).getName());
+//        intent.putExtra("Id",clist.get(position).getId());
+//        intent.putExtra("nonvegmeals",olist.get(position).getNonVegMeals()+"");
+//        intent.putExtra("vegmeals",olist.get(position).getVegMeals()+"");
+//        intent.putExtra("photoUrl",clist.get(position).getPhotoUrl());
+//        intent.putExtra("latitude",clist.get(position).getLatitude());
+//        intent.putExtra("longitude",clist.get(position).getLongitude());
+//
+//        System.out.println(clist.get(position)+" "+olist.get(position));
+//        this.startActivity(intent);
+//    }
 }
 
 
@@ -226,26 +259,33 @@ class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.V
     private List<Contributor> mData;
     private LayoutInflater mInflater;
     private Context ctx;
-    private ItemClickListener mClickListener;
+    //private ItemClickListener mClickListener;
     private Location myLoc= new Location("");
     private Location dest_location= new Location("");
     private static final DecimalFormat df = new DecimalFormat("0.00");
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference conRef = db.collection("Contributor");
+
+    ArrayList<AvailableFood> alAF;
+
 
     // data is passed into the constructor
-    MyRecyclerViewAdapter(Context context, List<AvailableFood> fdata,List<Contributor> cdata, Location myLocation) {
-        this.mInflater = LayoutInflater.from(context);
-        this.mData2 = fdata;
-        this.mData=cdata;
-        myLoc=myLocation;
+    MyRecyclerViewAdapter(ArrayList<AvailableFood> alAF, Context context) {
+        this.alAF = alAF;
         ctx=context;
     }
 
     // inflates the row layout from xml when needed
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.contributorlist_row, parent, false);
-        return new ViewHolder(view);
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View contactView = inflater.inflate(R.layout.contributorlist_row,parent,false);
+
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return  viewHolder;
     }
 
 
@@ -254,55 +294,113 @@ class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.V
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
 
-        for(int i=0;i<mData2.size();i++){
-            System.out.println(mData.get(position).getId()+" "+mData2.get(i).getCid());
-            if(mData.get(position).getId().equals(mData2.get(i).getCid())){
-                System.out.println(mData2.get(i).getNonVegMeals()+" "+mData2.get(i).getVegMeals());
-                if(mData2.get(i).getNonVegMeals()==0){
+        AvailableFood af = alAF.get(position);
 
-                    holder.redCircle.setVisibility(View.INVISIBLE);
+        String c_id = af.getCid();
+        final Contributor[] outer_c = new Contributor[1];
+        conRef.document(c_id).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if(value!=null && value.exists())
+                {
+                    Contributor c = value.toObject(Contributor.class);
+                    outer_c[0] = c;
+                    holder.nameView.setText(c.getName());
+                    holder.addressView.setText(c.getAddress());
+                    dest_location.setLatitude(Double.parseDouble(c.getLatitude()));
+                    dest_location.setLongitude(Double.parseDouble(c.getLongitude()));
+                    float distance = dest_location.distanceTo(myLoc);//in meters
+                    distance=distance/1000;
 
-                }
-                if(mData2.get(i).getVegMeals()==0){
+                    holder.distanceView.setText(distance+ " in mi.");
 
-                    holder.greenCircle.setVisibility(View.INVISIBLE);
-
-                }
-                dest_location.setLatitude(Double.parseDouble(mData.get(position).getLatitude()));
-                dest_location.setLongitude(Double.parseDouble(mData.get(position).getLongitude()));
-                float distance = dest_location.distanceTo(myLoc);//in meters
-                distance=distance/1000;
-                String name = mData.get(position).getName();
-                String address = mData.get(position).getAddress();
-                holder.nameView.setText(name);
-                holder.addressView.setText(address);
-                holder.distanceView.setText(df.format(distance)+" km");
-                Glide.with(ctx)
-                        .load(mData.get(position).getPhotoUrl())
+                    Glide.with(ctx)
+                        .load(c.getPhotoUrl())
                         .centerCrop()
                         .into(holder.imgView);
 
-                break;
+                    if(af.getNonVegMeals()==0){
+
+                        holder.redCircle.setVisibility(View.INVISIBLE);
+
+                    }
+                    if(af.getVegMeals()==0){
+
+                        holder.greenCircle.setVisibility(View.INVISIBLE);
+
+                    }
+                }
             }
-            else{
+        });
 
-                //Pass
 
+        holder.cvContributorOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ctx, "Time pass", Toast.LENGTH_SHORT).show();
+                ////        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+        Context context = view.getContext();
+        Intent intent = new Intent(context , RequestFoodActivity.class);
+        intent.putExtra("address",holder.addressView.getText().toString());
+        intent.putExtra("name",holder.nameView.getText().toString());
+        intent.putExtra("Id",c_id);
+        intent.putExtra("nonvegmeals",""+af.getNonVegMeals());
+        intent.putExtra("vegmeals",""+af.getVegMeals());
+        intent.putExtra("photoUrl",outer_c[0].getPhotoUrl());
+        intent.putExtra("latitude",outer_c[0].getLatitude());
+        intent.putExtra("longitude",outer_c[0].getLongitude());
+
+        ctx.startActivity(intent);
             }
+        });
 
-        }
+//        for(int i=0;i<mData2.size();i++){
+//            System.out.println(mData.get(position).getId()+" "+mData2.get(i).getCid());
+//            if(mData.get(position).getId().equals(mData2.get(i).getCid())){
+//                System.out.println(mData2.get(i).getNonVegMeals()+" "+mData2.get(i).getVegMeals());
+//                if(mData2.get(i).getNonVegMeals()==0){
+//
+//                    holder.redCircle.setVisibility(View.INVISIBLE);
+//
+//                }
+//                if(mData2.get(i).getVegMeals()==0){
+//
+//                    holder.greenCircle.setVisibility(View.INVISIBLE);
+//
+//                }
+//                dest_location.setLatitude(Double.parseDouble(mData.get(position).getLatitude()));
+//                dest_location.setLongitude(Double.parseDouble(mData.get(position).getLongitude()));
+//                float distance = dest_location.distanceTo(myLoc);//in meters
+//                distance=distance/1000;
+//                String name = mData.get(position).getName();
+//                String address = mData.get(position).getAddress();
+//                holder.nameView.setText(name);
+//                holder.addressView.setText(address);
+//                holder.distanceView.setText(df.format(distance)+" km");
+//                Glide.with(ctx)
+//                        .load(mData.get(position).getPhotoUrl())
+//                        .centerCrop()
+//                        .into(holder.imgView);
+//
+//                break;
+//            }
+//            else{
+//
+//                //Pass
+//
+//            }
+//
+//        }
 
     }
 
     // total number of rows
     @Override
     public int getItemCount() {
-
-
-        return mData2.size();
+        return alAF.size();
     }
     // stores and recycles views as they are scrolled off screen
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class ViewHolder extends RecyclerView.ViewHolder{
         TextView nameView;
         TextView addressView;
         TextView distanceView;
@@ -310,6 +408,8 @@ class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.V
         ImageView redCircle;
         ImageView greenCircle;
         ImageView imgView;
+
+        CardView cvContributorOrder;
 
 
         ViewHolder(View itemView) {
@@ -320,27 +420,12 @@ class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.V
             imgView=itemView.findViewById(R.id.icon);
             redCircle=itemView.findViewById(R.id.redCircle);
             greenCircle=itemView.findViewById(R.id.greenCircle);
-            itemView.setOnClickListener(this);
+            cvContributorOrder = itemView.findViewById(R.id.cvContributorOrder);
+            //itemView.setOnClickListener(this);
         }
 
-        @Override
-        public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
-        }
+
     }
 
-    // convenience method for getting data at click position
-    String getItem(int id) {
-        return mData.get(id).getName();
-    }
 
-    // allows clicks events to be caught
-    void setClickListener(ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
-    }
-
-    // parent activity will implement this method to respond to click events
-    public interface ItemClickListener {
-        void onItemClick(View view, int position);
-    }
 }
